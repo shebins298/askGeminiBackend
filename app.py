5381e91f98039c8d467c468746ba4727a590faf3
shebins298 <shebins298@gmail.com>
2025-04-13 19:25:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,6 @@ def generate():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
     
